32a13834fd5f7aeecb8e64cc0b4fd0dd894e4dbb
TonyBarbou <tonybarbou@live.fr>
2020-05-12 12:21:26 +0200

    Initial

---
+++ build/precache-manifest.81436e2895f1ce5fdd8047cb8d68d36a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d549cdf13f54890aee66a0ddbd65e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "022fe5c95d0132cd6239",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022fe5c95d0132cd6239",
+    "url": "/static/js/main.e9c61c3f.chunk.js"
+  },
+  {
+    "revision": "df16bcf85c25d69dad33",
+    "url": "/static/js/runtime-main.004a6136.js"
+  }
+]);
