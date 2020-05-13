09013a179fea2100cecde344ba7e078f996d50f7
TonyBarbou <tonybarbou@live.fr>
2020-05-13 23:53:21 +0200

    Updates

---
+++ precache-manifest.91c993a94c524492a8ae1a293a9a0eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c806bce762aee7d4e567ffee0e3918",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c9890ce9a2aa1b10918d",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fe2172f999527e202d5f",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.7d1e7194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9890ce9a2aa1b10918d",
+    "url": "/robofriends/static/js/main.75aa0044.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
